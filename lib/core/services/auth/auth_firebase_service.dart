55e8bb96df2e82c7204f07f375d430de16d306cc
Santiago Silvestre <silvestre.santiago732@gmail.com>
2024-07-17 01:25:20 -0300

    Configura firebase + create a service message

---
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class AuthFirebaseService implements AuthService {
+  static ChatUser? _currentUser;
+  // static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) async {
+    // _controller = controller;
+    final authChanges = FirebaseAuth.instance.authStateChanges();
+    await for (final user in authChanges) {
+      _currentUser = user == null ? null : _toChatUser(user);
+      controller.add(_currentUser);
+    }
+  });
+
+  @override
+  Stream<ChatUser?> get userChanges => _userStream;
+
+  @override
+  ChatUser? get currentUser => _currentUser;
+
+  @override
+  Future<void> login(String email, String password) async {
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  @override
+  Future<void> logout() async {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Future<void> singup(
+    String name,
+    String email,
+    String password,
+    File? image,
+  ) async {
+    final auth = FirebaseAuth.instance;
+    UserCredential credential = await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    if (credential.user == null) return;
+
+    // 1. upload da foto do usuário
+    final imageName = "${credential.user!.uid}.jpg";
+    final imageUserUrl = await _uploadUserImage(image, imageName);
+
+    // 2. atualizar os atributos do usuário
+    await credential.user?.updateDisplayName(name);
+    await credential.user?.updatePhotoURL(imageUserUrl);
+
+    // 3. salvar o user no banco de dados (opcional mas é legal implementar)
+    await _saveChatUser(_toChatUser(credential.user!, imageUserUrl));
+  }
+
+  Future<String?> _uploadUserImage(
+    File? image,
+    String imageName,
+  ) async {
+    if (image == null) return null;
+
+    final storage = FirebaseStorage.instance;
+    final imageRef = storage.ref().child('user_images').child(imageName);
+
+    await imageRef.putFile(image).whenComplete(() {});
+    return await imageRef.getDownloadURL();
+  }
+
+  Future<void> _saveChatUser(ChatUser user) async {
+    final firestore = FirebaseFirestore.instance;
+    final docRef = firestore.collection('users').doc(user.id);
+
+    return docRef.set({
+      'name': user.name,
+      'email': user.email,
+      'imageUrl': user.imageUrl,
+    });
+  }
+
+  static ChatUser _toChatUser(User user, [String? imageUrl]) {
+    return ChatUser(
+      id: user.uid,
+      name: user.displayName ?? user.email!.split('@')[0],
+      email: user.email!,
+      imageUrl: imageUrl ?? user.photoURL ?? 'assets/images/avatar.png',
+    );
+  }
+}
